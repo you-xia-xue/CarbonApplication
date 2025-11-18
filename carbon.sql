58433cab4fffb7d0b3971832f0252e8f7ca7a544
又吓雪 <3661445846@qq.com>
2025-11-18 14:11:38 +0800

    这个是sql文件

---
+++ carbon.sql
@@ -0,0 +1,222 @@
+/*
+ Navicat Premium Dump SQL
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 90500 (9.5.0)
+ Source Host           : localhost:3306
+ Source Schema         : carbon
+
+ Target Server Type    : MySQL
+ Target Server Version : 90500 (9.5.0)
+ File Encoding         : 65001
+
+ Date: 18/11/2025 14:10:34
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for energy_data
+-- ----------------------------
+DROP TABLE IF EXISTS `energy_data`;
+CREATE TABLE `energy_data`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `electricity` int NOT NULL,
+  `water` int NOT NULL,
+  `carbon` int NOT NULL,
+  `office_water` int NOT NULL,
+  `production_water` int NOT NULL,
+  `production_electricity` int NOT NULL,
+  PRIMARY KEY (`id` DESC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of energy_data
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for inventory_alert
+-- ----------------------------
+DROP TABLE IF EXISTS `inventory_alert`;
+CREATE TABLE `inventory_alert`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `material_id` int NOT NULL,
+  `material_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `warehouse_id` int NOT NULL,
+  `warehouse_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `current_stock` int NOT NULL,
+  `stock_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id` DESC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of inventory_alert
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for outbound_apply
+-- ----------------------------
+DROP TABLE IF EXISTS `outbound_apply`;
+CREATE TABLE `outbound_apply`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bill_no` int NOT NULL,
+  `related_bill_no` int NOT NULL,
+  `business_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `customer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `contract_no` int NOT NULL,
+  `contract_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `apply_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `applicant` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `apply_date` datetime NOT NULL,
+  `audit_date` datetime NOT NULL,
+  `audit_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id` DESC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of outbound_apply
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for production_data
+-- ----------------------------
+DROP TABLE IF EXISTS `production_data`;
+CREATE TABLE `production_data`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `product_id` int NOT NULL,
+  `product_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `model` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `specification` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `unit` int NOT NULL,
+  `stock` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `produced` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `outsourced` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `carbon_emission` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id` DESC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of production_data
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for purchase_contract
+-- ----------------------------
+DROP TABLE IF EXISTS `purchase_contract`;
+CREATE TABLE `purchase_contract`  (
+  ` id` int NOT NULL AUTO_INCREMENT,
+  `contract_no` int NOT NULL,
+  `apply_no` int NOT NULL,
+  `supplier` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `contact_person` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `amount` int NOT NULL,
+  `delivery_method` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `sign_date` datetime NOT NULL,
+  `applicant` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `apply_time` datetime NOT NULL,
+  `apply_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `auditor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `audit_time` datetime NOT NULL,
+  `audit_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (` id` DESC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of purchase_contract
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for purchase_plan
+-- ----------------------------
+DROP TABLE IF EXISTS `purchase_plan`;
+CREATE TABLE `purchase_plan`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bill_no` int NOT NULL,
+  `applicant` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `apply_date` datetime NOT NULL,
+  `apply_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `auditor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `audit_date` datetime NOT NULL,
+  `audit_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id` DESC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of purchase_plan
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sales_data
+-- ----------------------------
+DROP TABLE IF EXISTS `sales_data`;
+CREATE TABLE `sales_data`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `total_sales` double NOT NULL,
+  `contract_amount` double NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sales_data
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for sales_delivery
+-- ----------------------------
+DROP TABLE IF EXISTS `sales_delivery`;
+CREATE TABLE `sales_delivery`  (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `delivery_no` int NOT NULL,
+  `contract_no` int NOT NULL,
+  `customer_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `delivery_method` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `delivery_date` datetime NOT NULL,
+  `delivery_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `contact_person` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `contact_phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `out_stock_status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id` DESC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sales_delivery
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tender_info
+-- ----------------------------
+DROP TABLE IF EXISTS `tender_info`;
+CREATE TABLE `tender_info`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tender_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `end_date` datetime NOT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id` DESC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tender_info
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for tender_record
+-- ----------------------------
+DROP TABLE IF EXISTS `tender_record`;
+CREATE TABLE `tender_record`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `tender_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id` DESC) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tender_record
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
